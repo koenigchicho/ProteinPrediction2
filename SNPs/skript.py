733e790d774f92830d7ca2468429e62418fa5cc7
Madelina Giurgiu <mgiurgiu@i12k-biolab10.informatik.tu-muenchen.de>
2015-11-11 16:21:36 +0100

    changes skript.py

--- SNPs/skript.py
+++ SNPs/skript.py
@@ -7,7 +7,6 @@
 #>>> 
 #>>> print "Not mapped: " + str(dimRefProteome - len(fastaIDArray))
 #Not mapped: 215
-#>>> 
 
 #calling with SNAP2 Data and .fasta
 #
@@ -18,8 +17,14 @@
 #Not mapped: 0
 #>>> 
 
+######## Ignore X position
+# Total mapped sequnces: 20827
+# Not mapped: 55
+
+
 
 import os
+import re
 
 # Get Reference Proteome file
 filesRef = os.popen("wget -qO- ftp://ftp.ebi.ac.uk/pub/databases/reference_proteomes/QfO/Eukaryota/UP000005640_9606.fasta.gz | gunzip").read()
@@ -50,6 +55,30 @@ for i,item in enumerate(fileSnapArray):
 			if myseq in dict[keySeq][0]:
 				dict[keySeq][1] += 1 
 		
+filesSnap.close()
+
+# Map snap2 over Reference Proteome -Ignore X in reference
+print "start match"
+for i,item in enumerate(fileSnapArray):
+	if len(item)>0:
+		print i
+		myseq = os.popen("cat "+item+" | awk '{print $1}' | uniq |awk '{print substr($1,0,length($1)-1)}' | uniq |awk '{print substr($1,0,1)}' | paste -s -d \"\"").read()
+		myseq = myseq.replace("\n","")
+		for keySeq in dict:
+			if myseq in dict[keySeq][0] :
+				dict[keySeq][1] += 1 
+			else:
+				if len(myseq)==len(dict[keySeq][0]):
+					flag = 1
+					mismatchPosition = [i for i,(a1,b1) in enumerate(zip(myseq,dict[keySeq][0])) if b1 != a1]
+					for pos in mismatchPosition:
+						if dict[keySeq][0][pos] != 'X':
+							flag = 0
+							break
+					if flag == 1:
+						dict[keySeq][1] += 1 
+					
+
 ######## Print how many sequence matching only for .SNAP2 ########
 
 dimRefProteome = len(dict)
@@ -69,6 +98,7 @@ for key in fastaIDArray:
 		target.write("\n")
 		target.write(dict[key][0])
 		target.write("\n")
+
 target.close()
 
 
@@ -76,6 +106,7 @@ target = open('/mnt/home/student/mgiurgiu/protein_prediction/ProteinPrediction2/
 for key in fastaIDArray:
 	target.write(key)
 	target.write("\n")
+
 target.close()
 
 ################################ Get .fasta Files - Test if one of the proteins seq mapp directly to Reference proteome ###############################################################
@@ -93,7 +124,9 @@ for i,item in enumerate(fileFastaArray):
 		myseq = myseq.replace("\n","")
 		for keySeq in dict:
 			if myseq in dict[keySeq][0]:
-				dict[keySeq][1] += 1 	
+				dict[keySeq][1] += 1 
+	
+filesFasta.close()
 
 fastaIDArray = filter(lambda x:dict[x][1]>0, dict) # Fasta IDs for the already gemapped sequences
 print "Total mapped sequnces: " + str(len(fastaIDArray))
@@ -108,8 +141,43 @@ for key in fastaIDArray:
 		target.write(key)
 		target.write("\n")
 		target.write(dict[key][0])
-		target.write("\n")
 
 
 target.close()
+
+
+######################### Compare #########################
+
+files = os.popen("ls -1 | awk '$1 !~ /snap2/ && $1 ~ /UP0/ {print}'").read()
+fileArray = files.split('\n')
+dictSeq = {}
+for i,item in enumerate(fileArray):
+	if len(item)>0:
+		myseq = os.popen("cat "+item).read()
+		# skip header
+		myseqArr = myseq.split("\n")
+		# make one long string
+		id = myseqArr[0].replace("\n","")
+		seq = myseqArr[1].replace("\n","")
+		dictSeq[id]=seq
+files.close()
+
+out = open('/mnt/home/student/mgiurgiu/protein_prediction/ProteinPrediction2/SNPs/differences.fasta','w')
+for key in dict:
+	if dict[key][1] == 0:
+		out.write(key+'\n')
+		out.write(dict[key][0]+'\n\n')
+		out.write(dictSeq[key]+'\n\n\n')
+
+out.close()		
+
 print "finally done"
+
+
+ 
+    
+
+
+
+	
+
