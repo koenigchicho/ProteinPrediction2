2fe1fe2e3e751f2fe49984a9a341d6cafb615776
Madalina Giurgiu <giurgiu@braetling.cip.ifi.lmu.de>
2015-11-30 00:11:37 +0100

    ajust scripts

--- task4/countKeywords.py
+++ task4/countKeywords.py
@@ -1,15 +1,36 @@
 #!/usr/bin/Python
+# Compute Top 10 keys for the given Primary ACC 
 
 import os
+import sys
+import time
+
+# To set
+inputKeyFile = "mappedIDToKeywords.tab"
+outFileName = "countKeywordsDataFile.out"
+
+try:
+ 	opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile=","help="])
+except getopt.GetoptError:
+ 	print 'getKeys.py -i <keysFile> -o <outTop10File>'
+ 	sys.exit(2)
+for opt, arg in opts:
+	if opt in ("-h","--help"):
+    	print 'test.py -i <keysFile> -o <outTop10File>'
+    	sys.exit()
+	elif opt in ("-i", "--ifile"):
+    	inputKeysFile = arg
+	elif opt in ("-o", "--ofile"):
+		outFileName = arg
 
 # Convert mappedIDToKeywords
-os.popen("rm onlyKeywords.out")
-os.popen("cut -f 2- mappedIDToKeywords.tab | tail -n +2 > onlyKeywords.out")
+out = inputKeyFile + str(time.time())
+os.popen("cut -f 2- "+ inputKeyFile +" | tail -n +2 > " + out)
 
 # Dictionary with keywords
 keywordsDict = {}
 
-file = open('onlyKeywords.out','r')
+file = open(out,'r')
 
 for line in file:
 	lineArray = line.split(";")
@@ -24,14 +45,15 @@ for line in file:
 file.close()
 
 # Save counted keys in file
-writeFile = open('countKeywordsDataFile.out','w')
+outCountKeysFile  = inputKeyFile + str(time.time()) 
+writeFile = open(outCountKeysFile,'w')
 for k in keywordsDict:
 	writeFile.write(k+"\t"+str(keywordsDict[k])+"\n")
 
 writeFile.close()	
 
-os.system("cat countKeywordsDataFile.out | sort -k2 -n | tail -10 > top10Keys.out")
-
+os.system("cat "+outCountKeysFile+" | sort -k2 -n | tail -10 > " + outFileName)
+os.system("cat"+outCountKeysFile+" | awk '{s+=$2} END {print \"Others\t\"s}' >>" + outFileName) 
 
 
 
